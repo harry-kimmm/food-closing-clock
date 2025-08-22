ca2248397042e710f65f09d8145361e0f2cf738b
Harry Kim <hkim@Harrys-MacBook-Air-2.local>
2025-08-22 16:16:32 -0700

    Deploy via S3 and CloudFront

--- infra/lib/infra-stack.js
+++ infra/lib/infra-stack.js
@@ -1,23 +1,39 @@
-const { Stack, Duration } = require('aws-cdk-lib');
-// const sqs = require('aws-cdk-lib/aws-sqs');
+const { Stack, RemovalPolicy, CfnOutput } = require('aws-cdk-lib');
+const s3 = require('aws-cdk-lib/aws-s3');
+const cloudfront = require('aws-cdk-lib/aws-cloudfront');
+const origins = require('aws-cdk-lib/aws-cloudfront-origins');
 
 class InfraStack extends Stack {
-  /**
-   *
-   * @param {Construct} scope
-   * @param {string} id
-   * @param {StackProps=} props
-   */
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
+    const oai = new cloudfront.OriginAccessIdentity(this, 'SiteOAI');
+    siteBucket.grantRead(oai);
+
+    const distribution = new cloudfront.Distribution(this, 'SiteDist', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket, { originAccessIdentity: oai }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      errorResponses: [
+        { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html', ttl: undefined },
+      ],
+    });
+
+    // 4) Useful outputs for you to copy
+    new CfnOutput(this, 'CloudFrontURL', { value: `https://${distribution.domainName}` });
+    new CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+    new CfnOutput(this, 'SiteBucketName', { value: siteBucket.bucketName });
   }
 }
 
-module.exports = { InfraStack }
+module.exports = { InfraStack };
